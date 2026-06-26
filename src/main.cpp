9935d820d63cb70b799a411cbfe7848903d6c00c
BaptouDev <xu12.cb@gmail.com>
2026-06-26 16:14:47 +0200

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <include/entity.h>
 #include <include/player.h>
 #include <include/enemy.h>
+#include <include/json.hpp>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
 
 int main(void)
 {
@@ -13,6 +18,29 @@ int main(void)
     Player player = Player(Vector2f(0,0));
     Sprite map = Sprite("res/maps/test2/simplified/Level_0/_composite.png",Vector2f(0,0),4.0,0.0,Vector2f(0,0),720*752,0);
 
+    vector<PhysicsBody> map1col;
+
+    ifstream file("res/maps/test2/simplified/Level_0/Tiles1Layer.csv");
+    string line;
+    vector<vector<int>> int_map1;
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string field;
+        std::vector<int> row;
+        while (std::getline(ss, field, ',')) {
+            row.push_back(stoi(field));
+        }
+        int_map1.push_back(row);
+    }
+    for(int i=1;i<int_map1.size()-1;i++){
+        for(int j=1;j<int_map1.size()-1;j++){
+            if((int_map1[i][j] == 0)&&(int_map1[i-1][j]==1||int_map1[i-1][j-1]==1
+            ||int_map1[i][j-1]==1||int_map1[i+1][j-1]==1||int_map1[i+1][j]==1
+            ||int_map1[i+1][j+1]==1||int_map1[i][j+1]==1||int_map1[i-1][j+1]==1)){
+                
+            }
+        }
+    }
 
     Enemy enemy = Enemy(Vector2f(67,67));
 
@@ -22,13 +50,12 @@ int main(void)
     while (!WindowShouldClose())
     {
         dt = GetFrameTime();
-        player.update(dt,Vector2f(GetMousePosition().x,GetMousePosition().y));
+        player.update(dt,Vector2f(GetMousePosition().x,GetMousePosition().y)+camera_pos);
         enemy.get_player_pos(player.body.position);
         enemy.update(dt,Vector2f(GetMousePosition().x,GetMousePosition().y) );
 
         BeginDrawing();
 
-        player.update(dt,Vector2f(GetMousePosition().x,GetMousePosition().y)+camera_pos);
         camera_pos = player.sprite.origin + player.body.position -Vector2f(screenWidth,screenHeight)*(.5);
         BeginDrawing();
             ClearBackground(SKYBLUE);
